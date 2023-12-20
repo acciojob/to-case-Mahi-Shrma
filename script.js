42e7fb0466b45508dcc3b38ab9591f674c1da432
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-21 00:09:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function toCase(text) {
   // write your code here
-	let t1=text.toLowerCase();
-	let t2=text.toUpperCase();
-	let t3=t1+"-"+t2;
-	return t3;
+	// let t1=text.toLowerCase();
+	// let t2=text.toUpperCase();
+	// let t3=t1+"-"+t2;
+	// return t3;
+	let a=text.toLowerCase();
+	let b=text.toUpperCase();
+	let c=a.concat("-");
+	let d=c.concat(b);
+
+	return d;
 }
 
 // DO not change the code below
